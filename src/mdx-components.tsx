c39ca063613300d1935ff93bf7a6631a903a09af
J1nH4ng <2838080432@qq.com>
2025-05-23 13:21:55 +0800

    :construction:  feat: 添加网络内容

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,11 +4,13 @@ import { openapi } from "@/lib/source";
 import type { MDXComponents } from "mdx/types";
 import { Mermaid } from "@/components/mdx/mermaid";
 import * as Twoslash from "fumadocs-twoslash/ui";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...Twoslash,
+    ...TabsComponents,
     Mermaid,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
