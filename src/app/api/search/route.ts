febb0233e3fb16641cf54c25a495298d1946a6cf
J1nH4ng <j1nh4ng@icloud.com>
2025-06-09 15:47:30 +0800

    :sparkles: feat(search): 集成 Orama 中文分词器并优化搜索参数

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,16 @@
 import { source } from "@/lib/source";
 import { createFromSource } from "fumadocs-core/search/server";
+import { createTokenizer } from "@orama/tokenizers/mandarin";
 
 // it should be cached forever
 export const revalidate = false;
 
-export const { staticGET: GET } = createFromSource(source);
+export const { staticGET: GET } = createFromSource(source, {
+    components: {
+        tokenizer: createTokenizer(),
+    },
+    search: {
+        threshold: 0,
+        tolerance: 1,
+    }
+});
